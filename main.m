1f40e484c41f526cd74ca38000ae98256363a4b2
León E. Prieto Bailo <leon.enrique.prieto@estudiantat.upc.edu>
2023-11-19 18:06:40 +0100

    Reverted main comments.

--- main.m
+++ main.m
@@ -1,15 +1,15 @@
-% cd HORN\
-% main
-% cd ..
-% 
-% cd CORRUGATED_HORN\
-% main
-% cd ..
-% 
+cd HORN\
+main
+cd ..
+ 
+cd CORRUGATED_HORN\
+main
+cd ..
+ 
 cd CONICA\
 main
 cd ..
 
-% cd CORRUGATED_CONICA\
-% main
-% cd ..
+cd CORRUGATED_CONICA\
+main
+cd ..
